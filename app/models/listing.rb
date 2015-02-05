d496d57d1350c5849b86664b77a53da795499d1c
Tai Kyon <liquidtyde@gmail.com>
2015-02-05 19:44:43 +0000

    splitting up img storage for dev and prod

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,10 +1,14 @@
 class Listing < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, 
-  	:default_url => "default.jpg", 
-  	:storage => :dropbox,
-  	:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+  if Rails.env.development?
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+  else
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg", 
+    	:storage => :dropbox,
+    	:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+    	:path => ":style/:id_:filename"
+  end
   	
-###  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+# validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
 #	do_not_validate_attachment_file_type :image
 end
